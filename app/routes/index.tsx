4ec99f0ad5c05d3cf0e88de2cda07db5be731351
Masashi Fukuzawa <m.fukuzawa0316@gmail.com>
2024-04-28 19:08:25 +0900

    implement sample code

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,18 +1,25 @@
-import { css } from 'hono/css'
-import { createRoute } from 'honox/factory'
-import Counter from '../islands/counter'
+import { PrismaD1 } from '@prisma/adapter-d1';
+import { PrismaClient } from '@prisma/client';
+import { css } from 'hono/css';
+import { createRoute } from 'honox/factory';
+import Counter from '../islands/counter';
 
 const className = css`
   font-family: sans-serif;
-`
+`;
 
-export default createRoute((c) => {
-  const name = c.req.query('name') ?? 'Hono'
+export default createRoute(async (c) => {
+  const adapter = new PrismaD1(c.env.DB);
+  const prisma = new PrismaClient({ adapter });
+  const tenants = await prisma.tenant.findMany();
+
+  const name = c.req.query('name') ?? 'Hono';
+  const tenantName = tenants[0].name;
   return c.render(
     <div class={className}>
-      <h1>Hello, {name}!</h1>
+      <h1>Hello, {tenantName}!</h1>
       <Counter />
     </div>,
     { title: name }
-  )
-})
+  );
+});
